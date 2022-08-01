be920a18ef450c2932a1869aa6edc2ca11a6d3d1
KelvynLenis <kmartinslenis30@hotmail.com>
2022-08-01 09:23:09 -0300

    fixing bugs and uploading project

---
+++ src/components/ImproveSkills.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function ImproveSkills() {
+  const list = [
+    "learn new recipies",
+    "Experiment with food",
+    "Write your own recipies",
+    "Know nutrition facts",
+    "Get cooking tips",
+    "Get ranked",
+  ]
+
+  return (
+    <div className='section improve-skills'>
+      <div className="col img">
+        <img src="/img/gallery/img_10.jpg" alt="" />
+      </div>
+      <div className="col typography">
+        <h1 className="title">Improve Your Culinary Skills</h1>
+        { list.map((item, index) => (
+          <p className="skill-item" key={index}>{item}</p>
+        ))}
+        <button className="btn">signup now</button>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import ChefSection from '../components/ChefSection'
+import HeroSection from '../components/HeroSection'
+import ImproveSkills from '../components/ImproveSkills'
+import QuoteSection from '../components/QuoteSection'
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <ImproveSkills />
+      <QuoteSection />
+      <ChefSection />
+    </div>
+  )
+}
